ec222099b3888a607d72b19322cfb4095f73b21e
jerinchy09 <jerintasnimchy09@gmail.com>
2024-02-17 12:01:27 -0500

    first commit from maven

---
+++ src/main/java/jenkins_maven/jenkins_maven/App.java
@@ -0,0 +1,13 @@
+package jenkins_maven.jenkins_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/jenkins_maven/jenkins_maven/AppTest.java
@@ -0,0 +1,20 @@
+package jenkins_maven.jenkins_maven;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void Jenkins_maven()
+    {
+        System.out.println("Hi Jenkins from Maven");
+    }
+}
